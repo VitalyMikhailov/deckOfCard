1940265048a447fa07cd2fbf987538c6003d6e03
VitalyMikhailov <42186503+VitalyMikhailov@users.noreply.github.com>
2023-08-27 18:00:38 -0700

    delete old file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,70 +1,93 @@
 import * as requests from '../requests/requests'
-import testCases from '../fixtures/testCases.json'
+import cardsValue from '../fixtures/cardsValue.json'
 
-describe('Creating Decks of cards', () => {
+let piles = {
+    player1: "Bob",
+    player2: "Bill"
+}
+let cardsPlayer1
+let cardsPlayer2
+let player1Score = 0
+let player2Score = 0
+let drawnCards = []
+let cards = []
 
-  testCases.forEach((testCase) => {
-    const description = `Creating a Deck ${testCase.joker ? 'with' : 'without'} Joker and ${testCase.count} count`;
-    const shuffledDescription = `${description} (shuffled)`;
+describe('Creating new Deck, shuffle it, draw 3 cards for each player, check result', () => {
+    let deckId;
 
-    it(description, () => {
-      requests.createNewDeck(testCase.joker, testCase.count).should((response) => {
-        expect(response.status).to.eq(200);
-        if (response.body.error) {
-          expect(response.body.error).to.eq(testCase.error);
-        }
-        else {
-          expect(response.body.success).to.eq(testCase.success);
-          expect(response.body.shuffled).to.eq(testCase.shuffled);
-          expect(response.body.remaining).to.eq(testCase.remaining);
-        }
-      });
+    it('Creating the Deck', () => {
+        requests.createNewDeck(true, 1).then((response) => {
+            deckId = response.body.deck_id
+            expect(response.status).to.eq(200);
+            expect(response.body.success).to.eq(true)
+            expect(response.body.shuffled).to.eq(false)
+            expect(response.body.remaining).to.eq(54)
+
+        })
     });
 
-    it(shuffledDescription, () => {
-      requests.createNewDeckShuffled(testCase.joker, testCase.count).should((response) => {
-        if (response.body.error) {
-          expect(response.body.error).to.eq(testCase.error);
-        }
-        else {
-          expect(response.body.success).to.eq(testCase.success);
-          expect(response.body.shuffled).to.eq(testCase.shuffled !== true);
-          expect(response.body.remaining).to.eq(testCase.remaining);
-        }
-      });
+    it('Shuffle the Deck', () => {
+        requests.shuffleDeck(deckId).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.success).to.eq(true)
+            expect(response.body.remaining).to.eq(54)
+        })
     });
-  });
+
+    it('Draw 6 cards from the Deck', () => {
+        requests.drawCardsFromDeck(deckId, 6).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.success).to.eq(true);
+            expect(response.body.remaining).to.eq(48);
+            expect(response.body.cards).to.have.lengthOf(6)
+            for(let i = 0; i < 6; i++){
+                drawnCards[i] = response.body.cards[i].code
+            }
+        })
+    });
+
+    it('Creating first pile', () => {
+        cy.log(JSON.stringify(drawnCards))
+        cards = [drawnCards[0],drawnCards[1],drawnCards[2]]
+        requests.createPile(deckId, piles.player1, cards).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.success).to.eq(true)
+        })
+    });
+
+
+    it('Creating second pile', () => {
+        cards = [drawnCards[3],drawnCards[4],drawnCards[5]]
+        requests.createPile(deckId, piles.player2, cards).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.success).to.eq(true)
+        })
+    });
+
+    it('Listing cards from the first pile', () => {
+        requests.listPile(deckId, piles.player1).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.success).to.eq(true)
+            expect(response.body.piles).to.have.property(`${piles.player1}`)
+            cardsPlayer1 = response.body.piles[piles.player1].cards
+            cardsPlayer1.forEach((el)=>{
+                player1Score += cardsValue[el.code]
+             })
+            cy.log(player1Score)
+        })
+    });
+
+    it('Listing cards from the second pile', () => {
+        requests.listPile(deckId, piles.player2).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.success).to.eq(true)
+            expect(response.body.piles).to.have.property(`${piles.player2}`)
+            cardsPlayer2 = response.body.piles[piles.player2].cards
+            cardsPlayer2.forEach((el)=>{
+                player2Score += cardsValue[el.code]
+            })
+            cy.log(player2Score)
+        })
+    });
+
 })
-
-
-
-describe('Creating, shuffling a Deck. Drawing cards from the Deck', () => {
-  let deckId;
-
-  it('Creating the Deck with Jocker', () => {
-    requests.createNewDeck(true).should((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.success).to.eq(true)
-      expect(response.body.shuffled).to.eq(false)
-      deckId = response.body.deck_id
-    })
-  });
-
-  it('Shuffle Cards from the Deck', () => {
-    requests.shuffleDeck(deckId).should((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.success).to.eq(true)
-      expect(response.body.remaining).to.eq(54)
-    })
-  });
-
-  it('Draw 5 cards from the Deck', () => {
-    requests.drawCardsFromDeck(deckId, 5).should((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.success).to.eq(true);
-      expect(response.body.remaining).to.eq(49);
-      expect(response.body.cards).to.have.lengthOf(5)
-
-    })
-  });
-});
